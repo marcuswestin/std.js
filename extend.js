1c9c9175c1c7f738430f970863be76282453ff64
Marcus Westin <marcus.westin@gmail.com>
2011-04-04 22:56:09 -0400

    The function is called extend, not merge

--- extend.js
+++ extend.js
@@ -28,7 +28,7 @@
 	a.getBar() == 'sim'
 */
 
-module.exports = function merge(target, extendWith) {
+module.exports = function extend(target, extendWith) {
 	target = target || {}
 	for (var key in extendWith) {
 		if (typeof target[key] != 'undefined') { continue }
