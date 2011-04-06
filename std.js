7b5d4419b6d4c9d088d847053525e164dc4d8c5c
Marcus Westin <marcus.westin@gmail.com>
2011-04-06 00:51:20 -0400

    implement pick

--- std.js
+++ std.js
@@ -4,6 +4,7 @@ module.exports = {
 	isArray: require('./lib/isArray'),
 	each: require('./lib/each'),
 	map: require('./lib/map'),
+	pick: require('./lib/pick'),
 	extend: require('./lib/extend'),
 	pack: require('./lib/pack'),
 	unpack: require('./lib/unpack'),
