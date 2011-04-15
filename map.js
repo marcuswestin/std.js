7ae5872d9c0975f5726d479cc2be94f376433661
Marcus Westin <marcus.westin@gmail.com>
2011-04-15 11:08:11 -0700

    v0.1.16

--- map.js
+++ map.js
@@ -2,6 +2,10 @@ var each = require('./each')
 
 module.exports = function(items, ctx, fn) {
 	var result = []
+	if (!fn) {
+		fn = ctx
+		ctx = this
+	}
 	each(items, ctx, function(item, key) {
 		result.push(fn(item, key))
 	})
