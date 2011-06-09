4530197bbfc8bbedbd2f74db33178ad2325e7856
Marcus Westin <marcus.westin@gmail.com>
2011-06-09 16:55:03 -0700

    Whitespace and bug fix in client

--- client.js
+++ client.js
@@ -1,23 +1,24 @@
 module.exports = {
-  init: function() {
-  	function isClient(name) {
-  		var agent = window.navigator.userAgent,
-  			index = agent.indexOf(name)
-  		if (index < 0) { return false }
-  		this.version = parseInt(agent.substr(index + name.length + 1))
-  		return true
-  	}
-  	this.isIE = isClient('MSIE')
-  	this.isFirefox = isClient('Firefox')
-  	this.isWebKit = isClient('WebKit')
-  	this.isChrome = isClient('Chrome')
-  	this.isSafari = !this.isChrome && isClient('Safari')
-  },
-  
-  isQuirksMode: function(doc) {
-  	// in IE, if compatMode is undefined (early ie) or explicitly set to BackCompat, we're in quirks
-  	return this.isIE && (!doc.compatMode || doc.compatMode == 'BackCompat')
-  }
+	init: function() {
+		var client = this
+		function isClient(name) {
+			var agent = window.navigator.userAgent,
+				index = agent.indexOf(name)
+			if (index < 0) { return false }
+			client.version = parseInt(agent.substr(index + name.length + 1))
+			return true
+		}
+		client.isIE = isClient('MSIE')
+		client.isFirefox = isClient('Firefox')
+		client.isWebKit = isClient('WebKit')
+		client.isChrome = isClient('Chrome')
+		client.isSafari = !client.isChrome && isClient('Safari')
+	},
+
+	isQuirksMode: function(doc) {
+		// in IE, if compatMode is undefined (early ie) or explicitly set to BackCompat, we're in quirks
+		return this.isIE && (!doc.compatMode || doc.compatMode == 'BackCompat')
+	}
 }
 
 module.exports.init()
