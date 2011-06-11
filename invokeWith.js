0750103f0d9671a857cc35f7ee4331a5dd99e16f
Marcus Westin <marcus.westin@gmail.com>
2011-06-10 17:39:31 -0700

    bug fix - invokeWith was dropping the first curried argument

--- invokeWith.js
+++ invokeWith.js
@@ -8,7 +8,7 @@
 
 var slice = require('./slice')
 module.exports = function invokeWith(/*, curry1, ..., curryN */) {
-	var curryArgs = slice(arguments, 1)
+	var curryArgs = slice(arguments, 0)
 	return function futureInvocation(fn) {
 		var args = curryArgs.concat(slice(arguments, 1))
 		return fn.apply(this, args)
