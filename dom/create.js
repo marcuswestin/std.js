cba788c7b46b033a6eb7d8fc07355ceb37838fc9
Marcus Westin <marcus.westin@gmail.com>
2011-06-28 00:07:30 -0400

    Don't throw if properties is undefined

--- dom/create.js
+++ dom/create.js
@@ -1,10 +1,14 @@
 var each = require('../each'),
-  style = require('./style')
+	style = require('./style')
 
 module.exports = function create(tag, properties, doc) {
 	var element = (doc || document).createElement(tag)
-  if (properties.html) { element.innerHTML = properties.html; delete properties.html }
-  if (properties.style) { style(element, properties.style); delete properties.style }
-  each(properties, function(val, key) { element[key] = val })
+	
+	if (properties) {
+		if (properties.html) { element.innerHTML = properties.html; delete properties.html }
+		if (properties.style) { style(element, properties.style); delete properties.style }
+		each(properties, function(val, key) { element[key] = val })
+	}
+	
 	return element
 }
