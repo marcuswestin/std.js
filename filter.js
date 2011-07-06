24c6eb85eb22ceb66ce1cc82d39b49916dfc0508
Marcus Westin <marcus.westin@gmail.com>
2011-07-06 10:15:10 -0700

    Allow for specifying the context of the filter function in filter

--- filter.js
+++ filter.js
@@ -1,15 +1,22 @@
 /*
 	Example usage:
 	filter([1,2,0,'',false,null,undefined]) // -> [1,2,0,'',false]
-	filter([1,2,3], function(val, index) { val == 1 }) // -> [1]
+	filter([1,2,3], this, function(val, index) { val == 1 }) // -> [1]
 */
 var each = require('./each')
 
-module.exports = function filter(arr, fn) {
+module.exports = function filter(arr, ctx, fn) {
 	var result = []
-	if (!fn) { fn = falseOrTruthy }
+	if (arguments.length == 2) {
+		fn = ctx
+		ctx = this
+	}
+	if (!fn) {
+		fn = falseOrTruthy
+	}
+
 	each(arr, function(value, index) {
-		if (!fn(value, index)) { return }
+		if (!fn.call(ctx, value, index)) { return }
 		result.push(value)
 	})
 	return result
