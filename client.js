9cee5d2305d065d6a933a3a0de1db9bc2f300a68
Marcus Westin <marcus.westin@gmail.com>
2011-08-15 23:28:46 -0700

    make client a proper class, and add the beginnins to OS user-agent parsing

--- client.js
+++ client.js
@@ -1,29 +1,52 @@
-module.exports = {
-	init: function() {
-		var client = this
-		function isClient(name, versionString) {
-			var agent = window.navigator.userAgent,
-				index = agent.indexOf(name)
-			if (index < 0) { return false }
-			if (versionString) { index = agent.indexOf(versionString) }
-			client.version = parseFloat(agent.substr(index + (versionString || name).length + 1))
-			client.name = name
-			return true
-		}
-		client.isIE = isClient('MSIE')
-		client.isFirefox = isClient('Firefox')
-		client.isWebkit = client.isWebKit = isClient('WebKit')
-		client.isChrome = isClient('Chrome')
-		client.isSafari = !client.isChrome && isClient('Safari', 'Version')
-		client.isIPhone = isClient('iPhone', 'Version')
-		client.isIPad = isClient('iPad', 'Version')
-		client.isIPod = isClient('iPod', 'Version')
-	},
+var Class = require('./Class')
 
-	isQuirksMode: function(doc) {
+var Client = Class(function() {
+	
+	this.init = function(userAgent) {
+		this._userAgent = userAgent
+		this._parseClient()
+		this._parseOS()
+	}
+	
+	this._parseClient = function() {
+		(this.isChrome = this._isClient('Chrome'))
+			|| (this.isFirefox = this._isClient('Firefox'))
+			|| (this.isIE = this._isClient('MSIE'))
+			|| (this.isSafari = this._isClient('Safari', 'Version'))
+		
+		if (this.isSafari) {
+			(this.isIPhone = this._isClient('iPhone', 'Version'))
+				|| (this.isIPad = this._isClient('iPad', 'Version'))
+				|| (this.isIPod = this._isClient('iPod', 'Version'))
+		}
+	}
+	
+	this._parseOS = function() {
+		if (this.isSafari) {
+			this.isIOS = (this.isIPhone || this.isIPad || this.isIPod)
+			if (this.isIOS) { this.osVersion = parseFloat(this._userAgent.match(/ OS (\d+_\d+)/)[1].replace('_', '.'), 10) }
+		}
+		return this
+	}
+	
+	this.isQuirksMode = function(doc) {
 		// in IE, if compatMode is undefined (early ie) or explicitly set to BackCompat, we're in quirks
 		return this.isIE && (!doc.compatMode || doc.compatMode == 'BackCompat')
 	}
-}
+	
+	this._isClient = function(name, versionString) {
+		var agent = this._userAgent,
+			index = agent.indexOf(name)
+		if (index < 0) { return false }
+		if (versionString) { index = agent.indexOf(versionString) }
+		this.version = parseFloat(agent.substr(index + (versionString || name).length + 1))
+		this.name = name
+		return true
+	}
+	
+})
 
-module.exports.init()
+if (typeof window != 'undefined') { module.exports = new Client(window.navigator.userAgent) }
+else { module.exports = {} }
+
+module.exports.parse = function(userAgent) { return new Client(userAgent) }
