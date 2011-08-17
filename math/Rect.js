77b6af91da1f034e2f0e19092cac887a0620675b
Marcus Westin <marcus.westin@gmail.com>
2011-08-17 16:03:06 -0700

    Implement math.Rect

---
+++ math/Rect.js
@@ -0,0 +1,27 @@
+var Class = require('std/Class')
+
+module.exports = Class(function() {
+	
+	this.init = function(x, y, w, h) {
+		this.x = x
+		this.y = y
+		this._x2 = x + w
+		this._y2 = y + h
+	}
+	
+	this.pad = function(amount) {
+		this.x -= amount
+		this.y -= amount
+		this._x2 += amount
+		this._y2 += amount
+		return this
+	}
+	
+	this.containsPoint = function(point) {
+		log(this.x,' < ', point.x, ' && ', point.x, ' < ', this._x2)
+		log(this.y,' < ', point.y,' && ',point.y,' < ',this._y2)
+		return this.x < point.x && point.x < this._x2
+			&& this.y < point.y && point.y < this._y2
+	}
+	
+})
