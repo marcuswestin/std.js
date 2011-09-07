1bdcc7e26ddfc4975991acbd59c3cfb51b380b2f
Marcus Westin <marcus.westin@gmail.com>
2011-09-07 11:32:01 -0700

    Whitespace fix (no functional changes

--- Promise.js
+++ Promise.js
@@ -1,22 +1,22 @@
 var Class = require('./Class'),
-  bind = require('./bind'),
-  invokeWith = require('./invokeWith'),
-  slice = require('./slice')
+  	bind = require('./bind'),
+	invokeWith = require('./invokeWith'),
+	slice = require('./slice')
 
 module.exports = Class(function() {
-  this.init = function() {
-    this.dependants_ = []
-    this.fulfillment_ = null
-  }
-  
-  this.add = function(callback) {
-    if (this.fulfillment_) { callback.apply(this, this.fulfillment_) }
-    else { this.dependants_.push(callback) }
-  }
-  
-  this.fulfill = function(/* arg1, arg2, ...*/) {
-    if (this.fulfillment_) { throw new Error('Promise fulfilled twice') }
-    this.fulfillment_ = slice(arguments)
-    each(this.dependants_, invokeWith.apply(this, this.fulfillment_))
-  }
+	this.init = function() {
+		this.dependants_ = []
+		this.fulfillment_ = null
+	}
+
+	this.add = function(callback) {
+		if (this.fulfillment_) { callback.apply(this, this.fulfillment_) }
+		else { this.dependants_.push(callback) }
+	}
+
+	this.fulfill = function(/* arg1, arg2, ...*/) {
+		if (this.fulfillment_) { throw new Error('Promise fulfilled twice') }
+		this.fulfillment_ = slice(arguments)
+		each(this.dependants_, invokeWith.apply(this, this.fulfillment_))
+	}
 })
