5290c314d28e1fc445d7074c4de9f7acceae1b04
Marcus Westin <marcus.westin@gmail.com>
2011-11-26 15:30:36 -0800

    Make proto result invocations work without the new keyword

--- proto.js
+++ proto.js
@@ -1,8 +1,14 @@
 var each = require('std/each')
 module.exports = function proto(fn, properties) {
-	var prototype = fn.prototype
-	each(properties, function(prop, name) {
-		prototype[name] = prop
+	var prototype = fn.prototype,
+		F = function(args) { return fn.apply(this, args) }
+	
+	each(properties, function(prop, key) {
+		prototype[key] = prop
 	})
-	return fn
+	
+	F.prototype = prototype
+	return function() {
+		return new F(arguments)
+	}
 }
