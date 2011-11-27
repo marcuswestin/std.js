67d95fdd2786b05eb607d0ff0dd21291688291bd
Marcus Westin <marcus.westin@gmail.com>
2011-11-26 17:01:29 -0800

    Simplify proto significantly, and allow for nested prototype chains

--- proto.js
+++ proto.js
@@ -1,13 +1,8 @@
-var each = require('std/each')
 module.exports = function proto(fn, properties) {
-	var prototype = fn.prototype,
-		F = function(args) { return fn.apply(this, args) }
-	
-	each(properties, function(prop, key) {
-		prototype[key] = prop
-	})
-	
-	F.prototype = prototype
+	function F(args) {
+		return fn.apply(this, args)
+	}
+	F.prototype = properties
 	return function() {
 		return new F(arguments)
 	}
