dfb80de5e98845f3f0a604ca09c9f2e39a08dae1
Marcus Westin <marcus.westin@gmail.com>
2012-02-21 09:27:08 -0800

    Beef up proto - now a great way to build prototypical inheritence with objects that have have an instantiation function

--- proto.js
+++ proto.js
@@ -1,8 +1,42 @@
-module.exports = function proto(fn, properties) {
-	function F(args) {
-		return fn.apply(this, args)
+/* 
+	// Usage: proto(prototypeObj, intantiatingFn, properties)
+	
+	var base = {
+		say:function(arg) { alert(arg) }
 	}
-	F.prototype = properties
+
+	var person = proto(base,
+		function(name) {
+			this.name = name
+		}, {
+			greet:function(other) {
+				this.say("hello "+other.name+", I'm "+this.name)
+			}
+		}
+	)
+
+	var marcus = person("marcus"),
+		john = person("john")
+
+	marcus.greet(john)
+*/
+
+var create = require('std/create'),
+	each = require('std/each')
+
+var proto = module.exports = function proto(prototypeObject, instantiationFunction, propertiesToAdd) {
+	// F is the function that is required in order to implement JS prototypical inheritence
+	function F(args) {
+		// When a new object is created, call the instantiation function
+		return instantiationFunction.apply(this, args)
+	}
+	// The prototype object itself points to the passed-in prototypeObject,
+	// but also has all the properties enumerated in propertiesToAdd
+	F.prototype = create(prototypeObject)
+	each(propertiesToAdd, function(value, name) {
+		F.prototype[name] = value
+	})
+	
 	return function() {
 		return new F(arguments)
 	}
