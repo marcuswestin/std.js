bdad110770061efbb31d9075b4b8951967852bd0
Marcus Westin <marcus.westin@gmail.com>
2012-08-17 14:22:38 -0700

    make unique take a list of items and return a list where each item is represented only once

--- unique.js
+++ unique.js
@@ -1,8 +1,22 @@
+var identity = function(item) { return item }
+var itemId = function(item) { return item.id }
+
 /*
- * Return a globally unique string.
+ * Filters a list to unique items
  */
-module.exports = function() {
-	return module.exports.__namespace + (++module.exports.__id)
+module.exports = function(list, getId) {
+	if (!list || !list.length) { return [] }
+	if (!getId) {
+		getId = (list[0].id ? itemId : identity)
+	}
+	var seen = {}
+	var result = []
+	for (var i=0; i<list.length; i++) {
+		var item = list[i]
+		var id = getId(item)
+		if (seen[id]) { continue }
+		seen[id] = true
+		result.push(item)
+	}
+	return result
 }
-module.exports.__id = 0
-module.exports.__namespace = '__u'
