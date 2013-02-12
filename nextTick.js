dbf7e65667e477651850cccc3d88b4412b6cacf6
Marcus Westin <marcus.westin@gmail.com>
2013-02-12 11:28:49 -0800

    fix nextTick

--- nextTick.js
+++ nextTick.js
@@ -1,9 +1,11 @@
 module.exports = (function(global) {
-	if (global.process && global.process.nextTick) { return makeTicker(process.nextTick) }
+	if (typeof process != 'undfined' && process.nextTick) { return process.nextTick }
+	
 	var requestAnimationFrame = global.requestAnimationFrame || global.mozRequestAnimationFrame || global.webkitRequestAnimationFrame || global.msRequestAnimationFrame
 	if (requestAnimationFrame) { return makeTicker(requestAnimationFrame) }
+	
 	return makeTicker(setTimeout)
-
+	
 	function makeTicker(tickFn) {
 		return function nextTick(callback) {
 			tickFn(callback)
