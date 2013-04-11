157525ad6b86067f82680e22d90f807bcc29b0e7
Marcus Westin <marcus.westin@gmail.com>
2013-04-11 14:59:40 -0700

    make extend modify the original object. Useful for e.g. extend(function() { ... }, { foo:'bar' })

--- extend.js
+++ extend.js
@@ -28,10 +28,7 @@
 	a.getBar() == 'sim'
 */
 
-var copy = require('./copy')
-
 module.exports = function extend(target, extendWith) {
-	target = copy(target)
 	for (var key in extendWith) {
 		if (typeof target[key] != 'undefined') { continue }
 		target[key] = extendWith[key]
