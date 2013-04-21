c55ac49946ff23e4217a1c5a62c42b12f8075a8d
Marcus Westin <marcus.westin@gmail.com>
2013-04-20 18:20:05 -0700

    ensure that no values are declared twice in inverse()

--- inverse.js
+++ inverse.js
@@ -2,6 +2,9 @@ var each = require('std/each')
 
 module.exports = function inverse(obj) {
 	var result = {}
-	each(obj, function(val, key) { result[val] = key })
+	each(obj, function(val, key) {
+		if (result[val] !== undefined) { throw new Error("Duplicate value") }
+		result[val] = key
+	})
 	return result
 }
