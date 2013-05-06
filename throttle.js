72a475bc48c74e0259822c796d16e02e22220262
Marcus Westin <marcus.westin@gmail.com>
2013-05-05 18:27:28 -0700

    simplify throttle

--- throttle.js
+++ throttle.js
@@ -1,14 +1,12 @@
-var unique = 0
-module.exports = function throttle(fn, delay) {
-	if (typeof delay != 'number') { delay = 50 }
-	var timeoutName = '__throttleTimeout__' + (++unique)
+module.exports = function throttle(delay, fn) {
+	if (arguments.length == 1) {
+		fn = delay
+		delay = 0
+	}
+	var throttleTimeout
 	return function throttled() {
-		if (this[timeoutName]) { return }
-		var args = arguments, self = this
-		this[timeoutName] = setTimeout(function fireDelayed() {
-			delete self[timeoutName]
-			fn.apply(self, args)
-		}, delay)
-		fn.apply(self, args)
+		if (throttleTimeout) { return }
+		throttleTimeout = setTimeout(function() { throttleTimeout = null }, delay)
+		fn.apply(this, arguments)
 	}
 }
