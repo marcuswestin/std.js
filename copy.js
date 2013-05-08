5765b72a573412cd19badd12d77c32d88d07ec9d
Marcus Westin <marcus.westin@gmail.com>
2013-05-07 19:50:41 -0700

    better constructor usage

--- copy.js
+++ copy.js
@@ -2,7 +2,7 @@ var each = require('./each'),
 	isArray = require('./isArray')
 
 module.exports = function copy(obj, deep) {
-	var result = isArray(obj) ? [] : {}
+	var result = isArray(obj) ? new Array(obj.length) : {}
 	each(obj, function(val, key) {
 		result[key] = (deep && typeof val == 'object') ? copy(val, deep) : val
 	})
