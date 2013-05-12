dc3f46c274e6badc2f0f44876611b0f8d5b9c9c4
Marcus Westin <marcus.westin@gmail.com>
2013-05-12 11:19:40 -0700

    fix without again

--- without.js
+++ without.js
@@ -4,7 +4,7 @@ module.exports = function without(obj, prop) {
 	var res = {}
 	each(obj, function(val, key) {
 		if (key == prop) { return }
-		res[key] = prop
+		res[key] = val
 	})
 	return res
 }
