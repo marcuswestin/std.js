0c3e39b2d296c1c43c581e5c2f1555300ee602ba
Marcus Westin <marcus.westin@gmail.com>
2013-05-20 09:06:54 -0700

    this does not work for new Array(6).map...

--- map.js
+++ map.js
@@ -3,7 +3,6 @@ var isList = require('./isList')
 var isArray = require('./isArray')
 
 module.exports = function map(obj, fn) {
-	if (isArray(obj) && obj.map) { return obj.map(fn) }
 	var result = isList(obj) ? new Array(obj.length) : {}
 	each(obj, function(val, key) {
 		result[key] = fn(val, key)
