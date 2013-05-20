e91dea02dacaa020f91b06ff87d798c794c72d37
Marcus Westin <marcus.westin@gmail.com>
2013-05-20 09:08:56 -0700

    this does not work for new Array(1).forEach...

--- each.js
+++ each.js
@@ -3,12 +3,8 @@ var isList = require('./isList')
 module.exports = function each(items, fn) {
 	if (!items) { return }
 	if (isList(items)) {
-		if (items.forEach) {
-			items.forEach(fn)
-		} else {
-			for (var i=0; i < items.length; i++) {
-				fn(items[i], i)
-			}
+		for (var i=0; i < items.length; i++) {
+			fn(items[i], i)
 		}
 	} else {
 		for (var key in items) {
