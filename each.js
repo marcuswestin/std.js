f15ebb59fe3b2262b9aed6ba261debb42c28715d
Marcus Westin <marcus.westin@gmail.com>
2013-05-23 14:32:43 -0700

    use native map and forEach if available

--- each.js
+++ each.js
@@ -2,7 +2,9 @@ var isList = require('./isList')
 
 module.exports = function each(items, fn) {
 	if (!items) { return }
-	if (isList(items)) {
+	if (items.forEach == Array.prototype.forEach) {
+		items.forEach(fn)
+	} else if (isList(items)) {
 		for (var i=0; i < items.length; i++) {
 			fn(items[i], i)
 		}
--- map.js
+++ map.js
@@ -3,6 +3,9 @@ var isList = require('./isList')
 var isArray = require('./isArray')
 
 module.exports = function map(obj, fn) {
+	if (!obj) { return null }
+	if (obj.map == Array.prototype.map) { return obj.map(fn) }
+	
 	var result = isList(obj) ? new Array(obj.length) : {}
 	each(obj, function(val, key) {
 		result[key] = fn(val, key)
