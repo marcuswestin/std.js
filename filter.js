0e397e4b65e76270331f28336bf762a32dd63365
Marcus Westin <marcus.westin@gmail.com>
2013-05-30 15:42:38 -0700

    remove ctx from filter

--- filter.js
+++ filter.js
@@ -6,27 +6,21 @@
 var each = require('./each')
 var isArray = require('./isArray')
 
-module.exports = function filter(arr, ctx, fn) {
+module.exports = function filter(arr, fn) {
 	if (!arr) { return [] }
-	if (arguments.length == 2) {
-		fn = ctx
-		ctx = this
-	}
-	if (!fn) {
-		fn = falseOrTruthy
-	}
+	if (!fn) { fn = falseOrTruthy }
 	
 	var result
 	if (isArray(arr)) {
 		result = []
 		each(arr, function(value, index) {
-			if (!fn.call(ctx, value, index)) { return }
+			if (!fn(value, index)) { return }
 			result.push(value)
 		})
 	} else {
 		result = {}
 		each(arr, function(value, key) {
-			if (!fn.call(ctx, value, key)) { return }
+			if (!fn(value, index)) { return }
 			result[key] = value
 		})
 	}
