49e02fecba840e9a75469e218e700e40b1b7fd81
Marcus Westin <marcus.westin@gmail.com>
2013-05-30 15:38:36 -0700

    let 0 be a valid find

--- pickOne.js
+++ pickOne.js
@@ -6,12 +6,12 @@ module.exports = function pickOne(items, fn) {
 	if (isArray(items)) {
 		for (var i=0; i<items.length; i++) {
 			result = fn(items[i], i)
-			if (result != null && result != false) { return result }
+			if (result != null && result !== false) { return result }
 		}
 	} else {
 		for (var key in items) {
 			result = fn(items[key], key)
-			if (result != null && result != false) { return result }
+			if (result != null && result !== false) { return result }
 		}
 	}
 	return null
